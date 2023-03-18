ead387626f81123f6f4a8e0671456e001df66be9
Pathy <patittaoliver@gmail.com>
2023-03-18 13:40:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function carregaDicionario() {
+    var biografias = {
+        bio01: {
+            nome: "Marielle Franco",
+            descricao:
+                "Marielle Francisco da Silva, conhecida como Marielle Franco, foi uma socióloga, feminista, militante dos direitos humanos e política brasileira. Filiada ao Partido Socialismo e Liberdade (PSOL), elegeu-se vereadora do Rio de Janeiro para a Legislatura 2017-2020, durante a eleição municipal de 2016, com a quinta maior votação. Crítica da intervenção federal no Rio de Janeiro e da Polícia Militar, denunciava constantemente abusos de autoridade por parte de policiais contra moradores de comunidades carentes. Em 14 de março de 2018, foi assassinada a tiros.",
+            Imagem:
+                "https://i.postimg.cc/8fS8KSGX/marielle-franco.png",
+
+            Citacao:
+                "Quantos mais vão precisar morrer para que essa guerra aos pobres acabe?"
+        },
+
+        bio02: {
+            nome: "Lélia Gonzalez",
+            descricao:
+                "Lélia Gonzalez foi uma intelectual, política, professora e antropóloga brasileira.Ajudou a fundar instituições como o Movimento Negro Unificado (MNU), o Instituto de Pesquisas das Culturas Negras (IPCN), o Coletivo de Mulheres Negras N'Zinga e o Olodum. Sua militância em defesa da mulher negra levou-a ao Conselho Nacional dos Direitos da Mulher (CNDM), no qual atuou de 1985 a 1989. Foi candidata a deputada federal pelo PT, elegendo-se primeira suplente. Nas eleições seguintes, em 1986, candidatou-se a deputada estadual pelo PDT, novamente elegendo-se suplente.",
+            Imagem:
+                "https://i.postimg.cc/2q1f3GTG/lelia-gonzalez.png",
+            Citacao:
+                "A gente não nasce negro, a gente se torna negro. É uma conquista dura, cruel e que se desenvolve pela vida da gente afora."
+        },
+    }
+    console.log(biografias);
+}
+
+carregaDicionario();
